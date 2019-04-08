e9877d3614297f6739af652dc555b00e5d40a568
xrfang <xrfang@gmail.com>
2019-04-08 13:19:24 +0800

    完成users表的底层模型重构

--- src/clinica/h_users.go
+++ src/clinica/h_users.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"net/http"
-	"regexp"
-	"strconv"
-	"strings"
 )
 
 func users(w http.ResponseWriter, r *http.Request) {
@@ -28,53 +25,16 @@ func users(w http.ResponseWriter, r *http.Request) {
 			Users: listUsers(),
 		})
 	case "POST":
-		login := strings.TrimSpace(r.FormValue("login"))
-		if login == "" {
-			http.Error(w, "登录名(login)不能为空", http.StatusBadRequest)
-			return
-		}
-		rl := regexp.MustCompile(`(?i)^[a-z0-9]{1,16}$`)
-		if !rl.MatchString(login) {
-			http.Error(w, "登录名(login)格式不符合要求", http.StatusBadRequest)
-			return
-		}
-		role, err := strconv.Atoi(r.FormValue("role"))
-		if err != nil || role < -1 || role > 2 {
-			http.Error(w, "权限设置(role)错误", http.StatusBadRequest)
-			return
-		}
-		keys := []string{"login", "role"}
-		vals := []interface{}{login, role}
-		upds := `) ON CONFLICT(login) DO UPDATE SET role=?`
-		name := strings.TrimSpace(r.FormValue("name"))
-		if name != "" {
-			if len(name) > 32 {
-				http.Error(w, "姓名长度超过限制", http.StatusBadRequest)
-				return
-			}
-			keys = append(keys, "name")
-			vals = append(vals, name)
-			upds += `,name=?`
-		}
-		passwd := r.FormValue("passwd")
-		if passwd != "" {
-			keys = append(keys, "passwd")
-			vals = append(vals, HashPassword(passwd))
-			upds += `,passwd=?`
-		}
-		vals = append(vals, vals[1:]...)
-		cmd := `INSERT INTO users (` + strings.Join(keys, ",") + `) VALUES (?` +
-			strings.Repeat(",?", len(keys)-1) + upds
-		_, err = cf.dbx.Exec(cmd, vals...)
-		if err != nil {
-			http.Error(w, "内部错误: "+err.Error(), http.StatusInternalServerError)
-		}
+		r.ParseForm()
+		code, mesg := setUser(r.Form)
+		http.Error(w, mesg, code)
 	case "DELETE":
 		login := r.URL.Query().Get("login")
 		if login == u.Login {
 			http.Error(w, "不能删除当前用户", http.StatusForbidden)
 			return
 		}
-		cf.dbx.MustExec(`DELETE FROM users WHERE login=?`, login)
+		code, mesg := delUser(login)
+		http.Error(w, mesg, code)
 	}
 }
--- src/clinica/o_user.go
+++ src/clinica/o_user.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"database/sql"
+	"net/http"
+	"net/url"
+	"regexp"
+	"strconv"
+	"strings"
 
 	audit "github.com/xrfang/go-audit"
 )
@@ -36,4 +41,56 @@ func getUser(login, passwd string) *user {
 		return &u
 	}
 	return nil
+}
+
+func setUser(args url.Values) (int, string) {
+	arg := func(name string) string {
+		return strings.TrimSpace(args.Get(name))
+	}
+	login := arg("login")
+	if login == "" {
+		return http.StatusBadRequest, "登录名(login)不能为空"
+	}
+	rl := regexp.MustCompile(`(?i)^[a-z0-9]{1,16}$`)
+	if !rl.MatchString(login) {
+		return http.StatusBadRequest, "登录名(login)格式不符合要求"
+	}
+	role, err := strconv.Atoi(arg("role"))
+	if err != nil || role < -1 || role > 2 {
+		return http.StatusBadRequest, "权限设置(role)错误"
+	}
+	keys := []string{"login", "role"}
+	vals := []interface{}{login, role}
+	upds := `) ON CONFLICT(login) DO UPDATE SET role=?`
+	name := arg("name")
+	if name != "" {
+		if len(name) > 32 {
+			return http.StatusBadRequest, "姓名长度超过限制"
+		}
+		keys = append(keys, "name")
+		vals = append(vals, name)
+		upds += `,name=?`
+	}
+	passwd := args.Get("passwd")
+	if passwd != "" {
+		keys = append(keys, "passwd")
+		vals = append(vals, HashPassword(passwd))
+		upds += `,passwd=?`
+	}
+	vals = append(vals, vals[1:]...)
+	cmd := `INSERT INTO users (` + strings.Join(keys, ",") + `) VALUES (?` +
+		strings.Repeat(",?", len(keys)-1) + upds
+	_, err = cf.dbx.Exec(cmd, vals...)
+	if err != nil {
+		return http.StatusInternalServerError, "内部错误: " + err.Error()
+	}
+	return http.StatusOK, "OK"
+}
+
+func delUser(login string) (int, string) {
+	_, err := cf.dbx.Exec(`DELETE FROM users WHERE login=?`, login)
+	if err == nil {
+		return http.StatusOK, "OK"
+	}
+	return http.StatusInternalServerError, "内部错误: " + err.Error()
 }
