2191be9e339419bb09ac5ea0699cd6f4d355bbf3
xrfang <xrfang@gmail.com>
2019-05-21 22:54:21 +0800

    完成就诊记录编辑

--- src/clinica/routes.go
+++ src/clinica/routes.go
@@ -12,6 +12,7 @@ func setupRoutes() {
 	http.HandleFunc("/chpass", chpass)
 	http.HandleFunc("/api/listcases", apiListCases)
 	http.HandleFunc("/api/editcase", apiEditCase)
+	http.HandleFunc("/api/editconsult", apiEditConsult)
 	http.HandleFunc("/editcase", editCase)
 	http.HandleFunc("/patients", patients)
 }
